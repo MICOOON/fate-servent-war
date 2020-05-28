2b735565401fd18add942b31d83eb90338a89949
makoto <18457381307@163.com>
2020-05-29 01:45:48 +0800

    小兵移动

--- Assets/Scripts/SoldierFactory.cs
+++ Assets/Scripts/SoldierFactory.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class SoldierFactory : MonoBehaviour
 {
     [SerializeField]
-    private GameObject soldier;
+    private GameObject soldierPrefab;
 
     [SerializeField]
     private Transform birthplace;
+
+    [SerializeField]
+    private Transform soldierParent;
+
+    [SerializeField]
+    private Transform[] towers;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +28,12 @@ public class SoldierFactory : MonoBehaviour
         
     }
 
-    public void CreateSoldier() {
-        Instantiate(soldier, birthplace.position, Quaternion.identity);
+    public void CreateSoldier(Transform birthplace, Transform[] towers) {
+        GameObject soldier = Instantiate(soldierPrefab, birthplace.position, Quaternion.identity);
+        soldier.transform.parent = soldierParent;
+
+        SoldierMove soldierMove = soldier.GetComponent<SoldierMove>();
+        soldierMove.towers = towers;
     }
 
     public IEnumerator CreateSoldierGroup(float startTime, float singleTime, float groupTime) {
@@ -31,7 +42,7 @@ public class SoldierFactory : MonoBehaviour
         while (true) {
             for (int i = 0; i < 3; i++)
             {
-                CreateSoldier();
+                CreateSoldier(birthplace, towers);
 
                 yield return new WaitForSeconds(singleTime);
             }
---
+++ Assets/Scripts/SoldierMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SoldierMove : MonoBehaviour
+{
+    private NavMeshAgent nav;
+
+    private Animation ani;
+
+    public Transform target;
+
+    public Transform[] towers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animation>();
+        target = FindTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move() {
+        if (target == null)
+        {
+            target = FindTarget();
+            return;
+        }
+        ani.CrossFade("Run");
+        nav.SetDestination(target.position);
+    }
+
+    Transform FindTarget() {
+        for (int i = 0; i < towers.Length; i++)
+        {
+            if (towers[i] != null)
+            {
+                return towers[i];
+            }
+        }
+        return null;
+    }
+}
