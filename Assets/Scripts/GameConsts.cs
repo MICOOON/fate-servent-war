f60f8abb4032660767d2b8ebd7f469c61a3e34df
MICOOON <1830933670@qq.com>
2020-05-30 23:01:24 +0800

    防御塔攻击范围逻辑

---
+++ Assets/Scripts/GameConsts.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConsts
+{
+    public const string PLAYER = "Player";
+    public const string BLUE_TOWER = "BlueTower";
+    public const string RED_TOWER = "RedTower";
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    // 防御塔类型
     public int type;
+    // 将被防御塔攻击的英雄队列
+    public List<GameObject> beAttackedHeros;
+    // 将被防御塔攻击的小兵队列
+    public List<GameObject> beAttackedSoldiers;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (this.gameObject.tag.Equals("BlueTower"))
+        if (this.gameObject.tag.Equals(GameConsts.BLUE_TOWER))
         {
             type = (int) TowerType.BlueTower;
         }
-        else if (this.gameObject.tag.Equals("RedTower"))
+        else if (this.gameObject.tag.Equals(GameConsts.RED_TOWER))
         {
             type = (int) TowerType.RedTower;
         }
@@ -23,5 +28,37 @@ public class Tower : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag.Equals(GameConsts.PLAYER))
+        {
+            beAttackedHeros.Add(collider.gameObject);
+        }
+        else
+        {
+            SoldierBean soldierBean = collider.GetComponent<SoldierBean>();
+            if (soldierBean && this.type != soldierBean.type)
+            {
+                beAttackedSoldiers.Add(collider.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag.Equals(GameConsts.PLAYER))
+        {
+            beAttackedHeros.Remove(collider.gameObject);
+        }
+        else
+        {
+            SoldierBean soldierBean = collider.GetComponent<SoldierBean>();
+            if (this.type != soldierBean.type)
+            {
+                beAttackedSoldiers.Remove(collider.gameObject);
+            }
+        }
     }
 }
