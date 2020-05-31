ff78f9b13f463ca97c734b7c1090125c185852c4
MICOOON <1830933670@qq.com>
2020-05-31 23:08:59 +0800

    小兵与子弹消失

--- Assets/Scripts/BulletBean.cs
+++ Assets/Scripts/BulletBean.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class BulletBean : MonoBehaviour {
     private GameObject target;
     private float speed = 20F;
+    private Tower tower;
 
-    private void Start()
-    {
+    private void Start() {
+        tower = this.GetComponentInParent<Tower>();
         Destroy(this.gameObject, 1F);
     }
 
-    private void Update()
-    {
-        if (target)
-        {
+    private void Update() {
+        if (target) {
             Vector3 vector = target.transform.position - this.transform.position;
             this.GetComponent<Rigidbody>().velocity = vector.normalized * speed;
+        } else {
+            Destroy(this.gameObject);
         }
-        else
-        {
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.tag.Equals(GameConsts.SOLDIER)) {
+            tower.beAttackedSoldiers.Remove(collider.gameObject);
+            Destroy(collider.gameObject);
+            Destroy(this.gameObject);
+        } else if (collider.gameObject.tag.Equals(GameConsts.PLAYER)) {
+            tower.beAttackedHeros.Remove(collider.gameObject);
+            Destroy(collider.gameObject);
             Destroy(this.gameObject);
         }
     }
--- Assets/Scripts/GameConsts.cs
+++ Assets/Scripts/GameConsts.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameConsts {
+    // tag 常量
     public const string PLAYER = "Player";
     public const string BLUE_TOWER = "BlueTower";
     public const string RED_TOWER = "RedTower";
+    public const string SOLDIER = "Soldier";
 }
