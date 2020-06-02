a01c430afec470557aa7497eb6354224dd8e3da9
MICOOON <1830933670@qq.com>
2020-06-03 00:15:26 +0800

    “
    增加血条

---
+++ Assets/Scripts/HpScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpScript : MonoBehaviour {
+    [SerializeField]
+    private Transform hpPrefab;
+
+    private float hpValue;
+
+    public float HpValue {
+        get {
+            return hpValue;
+        }
+        set {
+            hpValue = value;
+            hpPrefab.localScale = new Vector3(hpValue, 1, 1);
+            hpPrefab.localPosition = new Vector3((1 - hpValue) * -0.8F, 0);
+        }
+    }
+}
