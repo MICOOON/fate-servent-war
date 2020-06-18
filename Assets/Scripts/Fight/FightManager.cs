c48d9a399085df949588be88213c06be82d9570d
MICOOON <1830933670@qq.com>
2020-06-18 20:40:16 +0800

    相机移动

--- Assets/Scripts/Fight/FightManager.cs
+++ Assets/Scripts/Fight/FightManager.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 
 public class FightManager : MonoBehaviour
 {
+    // 单例模式
+    public static FightManager instance;
+
+    // 主相机
+    private Camera cameraMain;
+
+    // 相机水平方向
+    private int cameraH;
+
+    // 相机垂直方向
+    private int cameraV;
+
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
+         cameraMain = Camera.main;
         // 加载场景
         this.WriteMessage(Protocol.TYPE_FIGHT, 0, FightProtocol.ENTER_CREQ, null);
     }
@@ -15,6 +29,43 @@ public class FightManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (cameraH) {
+            case 1:
+                if (cameraMain.transform.position.x <= 70) {
+                    cameraMain.transform.position = new Vector3(cameraMain.transform.position.x + cameraH, cameraMain.transform.position.y, cameraMain.transform.position.z);
+                }
+                break;
+            case -1:
+                if (cameraMain.transform.position.x >= -60) {
+                    cameraMain.transform.position = new Vector3(cameraMain.transform.position.x + cameraH, cameraMain.transform.position.y, cameraMain.transform.position.z);
+                }
+                break;
+        }
+        switch (cameraV) {
+            case 1:
+                if (cameraMain.transform.position.z <= 30) {
+                    cameraMain.transform.position = new Vector3(cameraMain.transform.position.x, cameraMain.transform.position.y, cameraMain.transform.position.z + cameraV);
+                }
+                break;
+            case -1:
+                if (cameraMain.transform.position.z >= -110) {
+                    cameraMain.transform.position = new Vector3(cameraMain.transform.position.x, cameraMain.transform.position.y, cameraMain.transform.position.z + cameraV);
+                }
+                break;
+        }
+    }
+
+    // 相机横移, 向右传1, 向左传-1
+    public void CameraHMove(int dir) {
+        if (cameraH != dir) {
+            cameraH = dir;
+        }
+    }
+
+    // 相机纵移, 向上传1, 向下传-1
+    public void CameraVMove(int dir) {
+        if (cameraV != dir) {
+            cameraV = dir;
+        }
     }
 }
---
+++ Assets/Scripts/Fight/SceneProcess.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SceneProcess : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = Input.mousePosition;
+        if (position.x < 10) {
+            // 通知相机向左移动
+            FightManager.instance.CameraHMove(-1);
+        } else if (position.x > Screen.width - 10) {
+            // 通知相机向右移动
+            FightManager.instance.CameraHMove(1);
+        } else {
+            // 不移动
+            FightManager.instance.CameraHMove(0);
+        }
+        if (position.y < 10) {
+            // 通知相机向下移动
+            FightManager.instance.CameraVMove(-1);
+        } else if (position.y > Screen.height - 10) {
+            // 通知相机向上移动
+            FightManager.instance.CameraVMove(1);
+        } else {
+            // 不移动
+            FightManager.instance.CameraVMove(0);
+        }
+        if (Input.GetKey(KeyCode.Space)) {
+
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+
+    }
+}
